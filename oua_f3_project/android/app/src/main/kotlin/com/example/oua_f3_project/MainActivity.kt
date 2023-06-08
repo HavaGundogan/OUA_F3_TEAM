fc53803dfe3674f2310efe472299e2138dac275a
Hava Gundogan <havakabasgundogan@gmail.com>
2023-06-08 22:16:52 +0300

    update oua_f3_projects

--- oua_f3_project/android/app/src/main/kotlin/com/example/oua_f3_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.oua_f3_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ oua_f3_projects/android/app/src/main/kotlin/com/example/oua_f3_projects/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oua_f3_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
