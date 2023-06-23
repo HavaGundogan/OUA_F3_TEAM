0512367a6e875ff7b4e27ee5888577b9767dbdeb
Ulaş <ulas_bjk1903@hotmail.com>
2023-06-23 21:53:19 +0300

    new

--- lib/ui/main/main_view.dart
+++ lib/ui/main/main_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:yourself_in_time_project/core/di/get_it.dart';
+import 'package:yourself_in_time_project/core/services/auth_service.dart';
 import 'package:yourself_in_time_project/ui/main/main_view_model.dart';
 
 class MainView extends StatelessWidget {
@@ -8,9 +9,20 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AuthService _auth = AuthService();
     return ViewModelBuilder<MainViewModel>.reactive(
         viewModelBuilder: () => getIt<MainViewModel>(),
         disposeViewModel: false,
-        builder: (context, model, child) => Scaffold());
+        builder: (context, model, child) => Scaffold(
+              body: Center(
+                child: FloatingActionButton(
+                  onPressed: () {
+                    _auth.signOut();
+                    model.init();
+                  },
+                  child: Center(child: Text("Çıkış Yap")),
+                ),
+              ),
+            ));
   }
 }
--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,5 +1,8 @@
+import 'package:yourself_in_time_project/app/app.router.dart';
 import 'package:yourself_in_time_project/app/app_base_view_model.dart';
 
 class MainViewModel extends AppBaseViewModel {
-  init() {}
+  init() {
+    navigationService.navigateTo(Routes.loginView);
+  }
 }
