0c4ffa9b0b74e3bebc975ed68c5275c17c99778e
Ulaş <ulas_bjk1903@hotmail.com>
2023-06-23 23:49:26 +0300

    new

--- lib/common/widgets/password_textfield_widget.dart
+++ lib/common/widgets/password_textfield_widget.dart
@@ -2,44 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:yourself_in_time_project/common/constants/colors_constants.dart';
 import 'package:yourself_in_time_project/common/widgets/email_login_widget.dart';
 
-class PasswordTextFieldWidget extends StatelessWidget {
+class PasswordTextFieldWidget extends StatefulWidget {
   const PasswordTextFieldWidget({
-    super.key,
+    Key? key,
     required TextEditingController passwordController,
     required this.widget,
-  }) : _passwordController = passwordController;
+  })  : _passwordController = passwordController,
+        super(key: key);
 
   final TextEditingController _passwordController;
   final EmailAndPasswordWidget widget;
 
   @override
+  _PasswordTextFieldWidgetState createState() =>
+      _PasswordTextFieldWidgetState();
+}
+
+class _PasswordTextFieldWidgetState extends State<PasswordTextFieldWidget> {
+  bool _isPasswordVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          controller: _passwordController,
-          onSaved: (value) {
-            widget.password = value!;
+    return TextFormField(
+      obscureText: !_isPasswordVisible,
+      controller: widget._passwordController,
+      onSaved: (value) {
+        widget.widget.password = value!;
+      },
+      decoration: InputDecoration(
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
           },
-          decoration: InputDecoration(
-            icon: Icon(
-              Icons.vpn_key,
-              color: ColorConstants.loginColor,
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: ColorConstants.greyColorShade),
-            ),
-            labelText: "Password",
-            enabledBorder: InputBorder.none,
-            labelStyle: TextStyle(color: ColorConstants.greyColor),
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            color: ColorConstants.greyColor,
           ),
-          validator: (value) {
-            if (value!.isEmpty) {
-              return "Password cannot be empty!";
-            } else {}
-          },
         ),
-      ],
+        icon: Icon(
+          Icons.vpn_key,
+          color: ColorConstants.loginColor,
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: ColorConstants.greyColorShade),
+        ),
+        labelText: "Password",
+        enabledBorder: InputBorder.none,
+        labelStyle: TextStyle(color: ColorConstants.greyColor),
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Password cannot be empty!";
+        }
+      },
     );
   }
 }
